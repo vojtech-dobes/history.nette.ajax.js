188a9e45ce9e3c6a709cd5cc4473b2971102790d
Radek Šerý <radek.sery@peckadesign.cz>
2017-01-24 14:05:05 +0100

    - fixing condition in popstate event handler; since `e` is jQuery Event Object, there is no `state` property defined, we need to use `e.originalEvent.state` instead

--- client-side/history.ajax.js
+++ client-side/history.ajax.js
@@ -48,7 +48,7 @@ $.nette.ext('history', {
 			var state = e.originalEvent.state || this.initialState;
 			var initialPop = (!this.popped && initialUrl === state.href);
 			this.popped = true;
-			if (initialPop || !e.state) {
+			if (initialPop || !e.originalEvent.state) {
 				return;
 			}
 			if (this.cache && state.ui) {
